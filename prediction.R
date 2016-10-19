4cb7ca5df9fc859fe366515cf0ea85f635cd2b70
Vivek Kumar Singh <vksingh.iiitb@gmail.com>
2016-10-19 12:08:51 -0400

    Update prediction.R

--- prediction.R
+++ prediction.R
@@ -3,9 +3,12 @@ bitcoin<-read.csv(file.choose(),header = T)
 #bitcoin_jan_15_to_june_16.csv . Its in box folder
 bitcoin$Date<-as.Date(bitcoin$Date)
 
+#training till may
 training<-bitcoin[bitcoin$Date<='2016-05-31',]
+#testing for june
 test<-bitcoin[bitcoin$Date>'2016-05-31',]
 
+#Autoregressive model
 arModel<-ar(training$Close.Price)
 arModelPrediction<-predict(arModel,n.ahead = 29)
 
@@ -14,7 +17,7 @@ plot(test$Close.Price,type="b",ylim = c(1,1000),xlab = "Hours",
      ylab = "USD", main = "Prediction for July 2016")
 lines(as.numeric(arModelPrediction$pred),pch=2,type = "b")
 
-
+#There are different algorithms for AR model. In the following code, we evaluate different algorithms
 arModel<-ar(training$Close.Price,model="yule-walker")
 arModelPrediction<-predict(arModel,n.ahead = 29)
 lines(as.numeric(arModelPrediction$pred),pch=3,type = "b")
@@ -41,7 +44,9 @@ legend("bottom",c("Actual","AR prediction",
                   "AR-yule-walker","AR-Burg","AR-ols","AR-mle",
                   "AR-yw"),pch=c(1,2,3,4,5,6,7))
 
+#I have stored the plot in the the box folder: bitcoin/results
 
+#Function to calculate RMSE
 rmse<-function(x,y)
 {
   sqrt(sum((x-y)^2)/length(x))
