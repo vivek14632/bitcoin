d97ab1feb000a9a2ec0f8912cb9a13429cc2bb03
Vivek Kumar Singh <vksingh.iiitb@gmail.com>
2016-11-27 17:47:34 -0500

    Update facebook.R

--- facebook.R
+++ facebook.R
@@ -4,21 +4,66 @@ library('Rfacebook')
 
 #create a favebook app and add http://localhost:1410/ to your app
 fb_oauth <- fbOAuth(app_id="***",
-                    app_secret="****")
-
-save(fb_oauth, file="fb_oauth")
-load("fb_oauth")
-me <- getUsers("me", token=fb_oauth)
+                    app_secret="***")
 
 
+fb_page <- getPage(page="narendramodi", token=fb_oauth,n=1000,feed = T,reactions = T)
 
-dd=getFriends(token=fb_oauth)
-fb_page <- getPage(page="GreenBenchBrewing", token=fb_oauth,n=1000)
+#emotions
+fb_page$love_count
+fb_page$haha_count
+fb_page$wow_count
+fb_page$sad_count
+fb_page$angry_count
+
+
+
+fb_page$message
 fb_page$likes_count
-plot(fb_page$likes_count[1:100],type="b")
-lines(fb_page$comments_count[1:100],col="blue")
-lines(fb_page$shares_count[1:100],col="red")
 
-summary(lm(fb_page$shares_count~fb_page$likes_count+fb_page$comments_count))
+par(mfrow=c(3,1))
 
+
+plot(log(fb_page$likes_count),type="l")
+plot(log(fb_page$shares_count),col="red",type = "b")
+plot(log(fb_page$comments_count),col="red",type = "b")
+
+
+cor(fb_page$shares_count,fb_page$comments_count)
+summary(lm(fb_page$shares_count~
+             fb_page$likes_count+
+             fb_page$comments_count+
+             fb_page$love_count+
+           fb_page$haha_count+
+           fb_page$wow_count+
+           fb_page$sad_count+
+           fb_page$angry_count))
+
+library('foreign')
+library('MASS')
+install.packages('RFGLS',dependencies = T)
+library('RFGLS')
+summary(fgls(fb_page$shares_count~
+             fb_page$likes_count+
+             fb_page$comments_count+
+             fb_page$love_count+
+             fb_page$haha_count+
+             fb_page$wow_count+
+             fb_page$sad_count+
+             fb_page$angry_count))
+
+
+fb_page$likes_count[1]
+fb_page$love_count[1]+
+fb_page$haha_count[1]+
+fb_page$wow_count[1]+
+fb_page$sad_count[1]+
+fb_page$angry_count[1]
+
+
+fb_page$love_count[1]
+  fb_page$haha_count[1]
+  fb_page$wow_count[1]
+  fb_page$sad_count[1]
+  fb_page$angry_count[1]
 
